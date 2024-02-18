49f1064791eea3daa9ee44f3a8d3c3d6cebc41dc
gssantaella <gssantaella@gmail.com>
2024-02-18 09:18:20 -0300

    temos materiais

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include "src/rtweekend.h"
 
 #include "src/camera.h"
+#include "src/color.h"
 #include "src/hittable_list.h"
+#include "src/material.h"
 #include "src/sphere.h"
 
 
@@ -12,9 +14,17 @@ int main() {
 
     hittable_list world;
 
-    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
-    world.add(make_shared<sphere>(point3(0.9, 0, -1), 0.3));
-    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+    auto material_ground = make_shared<lambertian>(color(0.3, 0.3, 0.8));
+    auto material_center = make_shared<metal>(color(0.7, 0.3, 0.3));
+    auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8));
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto material_up = make_shared<metal>(color(0.2, 0.6, 0.2));
+
+    world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(1.0, -0.3, -0.8), 0.25, material_right));
+    world.add(make_shared<sphere>(point3(0.0, 1.0, -0.5), 0.25, material_right));
 
     camera cam;
 
